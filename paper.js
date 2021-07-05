4da60ccbd08710b018f60d8d4daaf6774153e8ca
hardik466 <84703788+hardik466@users.noreply.github.com>
2021-07-05 08:26:12 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,15 @@
+class paper {
+    constructor(){
+        var options = {
+            isStatic : false,
+            restitution : 0.3,
+            friction : 0,
+            density : 1.2
+         }
+        display()
+        {
+            this.image = loadImage("images/paper.png");
+
+        }
+    }
+}image = loadImage("images/paper.png")
